70a50548cf0f9d2cc5a00bb3d3816012faf43774
gujejianig <100591700+gujejianig@users.noreply.github.com>
2022-03-14 18:57:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,215 @@
-var input =  document.querySelector('.textInput');
-var addBtn = document.querySelector('.btn-primary');
-var parentDiv = document.getElementById('mainContent');
-window.addEventListener('load', () =>  {
-   
+const addTaskInput = document.getElementById("add-task-input");
+const taskList = document.getElementById("tasks-list");
+const paginationBtnWrapper = document.getElementById(
+    "pagination-buttons-wrapper"
+);
 
-   function start() {
-       
-       const inputValue = input.value
-       if (inputValue.length < 1 ) {
-           alert('input must filledalert out')
-       }
+let getIndex;
 
-else {
-    const listContainer = document.createElement('div');
-    listContainer.classList.add('listContainer')
+let initialTaskData = [];
 
-    parentDiv.appendChild(listContainer)
+let pagesData = [];
 
-    const content = document.createElement('div')
-    content.classList.add('content')
-    listContainer.appendChild(content)
+const showTask = () => {
+    const innerDiv = document.createElement("div");
+    innerDiv.addEventListener("click", () => {
+    });
+
+    let indexControl = 0
+    let counter = 0
 
 
-    const inputVal = document.createElement('input')
+    counter = initialTaskData.length
 
-    inputVal.classList.add('newInput')
-    inputVal.type = 'text'
-    inputVal.value = inputValue;
-    inputVal.setAttribute('readOnly', 'readOnly')
+    console.log(counter)
 
-    content.appendChild(inputVal)
+    if(counter > 5) {
+        indexControl = indexControl + 5; // როდის უნდა გაიზარდოს?!?!?!?!
+        counter = 0
 
-
-    const removeBtn = document.createElement('button')
-    removeBtn.classList.add('btn-danger')
-    removeBtn.innerText = 'Remove'
-
-    const editBtn = document.createElement('button')
-    editBtn.classList.add('btn-info')
-    editBtn.innerText = 'Edit'
-
-    const checkBox = document.createElement('input')
-    checkBox.type = 'checkbox'
-
-    checkBox.addEventListener('click', () => {
-        if (checkBox.checked == true) {
-            inputVal.classList.add('checkControler')
-            editBtn.disabled = true
-            editBtn.innerHTML = 'Edit'
-            removeBtn.innerHTML = 'Remove'
-
-        } else {
-            inputVal.classList.remove('checkControler')
-            editBtn.disabled = false
-
-        }
-    })
-    
-
-    content.appendChild(checkBox)
-    content.appendChild(removeBtn)
-    content.appendChild(editBtn)
-
-
-    var arr = []
-
-    arr.push(inputValue)
-    var data = [...arr, inputValue]
-
-    console.log(data)
-
-
-    
-    removeBtn.addEventListener('click', (e) => {
-    if (removeBtn.innerText == 'Remove') {
-        parentDiv.removeChild(listContainer)
-       
-    } else if (removeBtn.innerText == 'Cancel') {
-        editBtn.innerText = 'Edit'
-        removeBtn.innerText = 'Remove'
-        inputVal.value = inputValue
     }
-})
-    editBtn.addEventListener('click', () => {
-        if(editBtn.innerHTML == 'Edit') {
-            editBtn.innerText = 'Save'
-            removeBtn.innerText = 'Cancel'
-            inputVal.removeAttribute('readOnly')
-            inputVal.focus()
+
+    for(let i = 0; i < 6; i++) {
+        console.log(i)
+
+    }
+
+
+
+    // indexcontrol უნდა გაიზაროდს 5 ით, როდესაც index 5-ს მიაღწევს/
+    // index უნდა იზრდებოდეს initialTaskData.length - ის მეშვეობით (მასზე უნდა იყოს დამოკიდებული)
+    // index როცა მიაღწევს 5 მდე, უნდა განულდეს//////
+
+    // როგორ გავიგოთ, როდის უნდა განულდეს ინდექსი? ----->  arr.length = 5, 10 15 <-----
+
+
+
+    if (initialTaskData.length >=0) {
+        taskList.innerHTML =
+            initialTaskData.slice(indexControl, indexControl + 5)
+                ?.map((task, index) => {
+
+                    getIndex = index;
+                    return `<div id="mainDiv${task.id}" class="task" > <p id="changedValue${task.id}" class="todoInput">${task.task}  </p>
+                <input onclick=onCheckBox(${task.id}) id="checkBox${task.id}" type="checkbox" /> 
+                <input value="${task.task}" id="ident${task.id}" class="hideItem"/>
+                <button onclick=removeBtn(${task.id}) id="remove${task.id}"  type="button" class="btn btn-danger">Remove</button>
+                <button onclick=editHandler(${task.id})  type="button" id="edit${task.id}" class="btn btn-info">Edit</button></div>`;
+                })
+                .join("")}
+    if ( pagesData[pagesData.length - 1] === undefined) {
+        taskList.innerHTML = "<div>No todo exist</div>"
+    }
+};
+
+const addTask = () => {
+
+    // console.log(initialTaskData)
+
+    if (addTaskInput.value.trim().length !== 0) {
+        initialTaskData.push({task: addTaskInput.value, id: Date.now()});
+        addTaskInput.value = "";
+        renderPagesData(initialTaskData);
+    }
+
+    const lastElement = paginationBtnWrapper.lastChild;
+    if (lastElement) {
+        lastElement.classList.add("active");
+    }
+
+    onPageControler = Number(lastElement.textContent);
+};
+
+let onPageControler;
+
+const changePage = (pageNumber) => {
+    const paginationBtn = document.getElementById(`paginationBtn${pageNumber}`);
+    const globalPaginationBtn = document.querySelectorAll(".btn-default");
+
+    if (paginationBtn) {
+        globalPaginationBtn.forEach((button) => {
+            button.classList.remove("active");
+
+        });
+        onPageControler = paginationBtn.innerHTML;
+        if (Number(paginationBtn.innerHTML) === pageNumber + 1) {
+            paginationBtn.classList.add("active");
         }
-        else {
-            editBtn.innerText = 'Edit'
-            removeBtn.innerText = 'Remove'
-            inputVal.setAttribute('readOnly', 'readOnly')
-        } 
-    })}
-   }
 
-   addBtn.addEventListener('click', (e) => {
-       e.preventDefault()
-       start()
-       input.value = ''
-       input.focus()
-   })
-    
-    input.addEventListener('keyup', (e) => {
-        if(e.keyCode == 13) {
-            start()
-            input.value = ''
-        }      
-    },
-    )
- }
-)
+    } else {
+        const lastElement = paginationBtnWrapper.lastChild;
 
+        if (lastElement) {
+            let akk = lastElement.textContent;
+            onPageControler = Number(akk);
+            lastElement.classList.add("active");
+        }
+
+
+    }
+    if (initialTaskData.length >= 0) {
+        taskList.innerHTML = pagesData[pageNumber]
+            .map((task) => {
+                return `<div id="mainDiv${task.id}" class="task" > <p id="changedValue${task.id}" class="todoInput">${task.task}  </p>
+                <input onclick=onCheckBox(${task.id}) id="checkBox${task.id}" type="checkbox" /> 
+                <input value="${task.task}" id="ident${task.id}" class="hideItem"/>
+                <button onclick=removeBtn(${task.id}) id="remove${task.id}"  type="button" class="btn btn-danger">Remove</button>
+                <button onclick=editHandler(${task.id})  type="button" id="edit${task.id}" class="btn btn-info">Edit</button></div>`;
+            })
+            .join("");
+    }
+};
+
+addTaskInput.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        addTask();
+        addTaskInput.value = "";
+    }
+});
+
+const renderPagesData = (initialData) => {
+    let singlePageData = [];
+    let counter = 0;
+
+    pagesData = [];
+    initialData.forEach((task, index) => {
+        counter++;
+        singlePageData.push(task);
+        if (counter === 5 || index === initialData.length - 1) {
+            pagesData.push(singlePageData);
+            counter = 0;
+            singlePageData = [];
+        }
+
+
+    });
+
+    paginationBtnWrapper.innerHTML = pagesData
+        .map((singlePageList, index) => {
+            return (
+                `<button id="paginationBtn${index}" class="btn btn-default" onclick=changePage(${index})> 
+                ${index + 1}
+                </button>`
+            );
+        })
+        .join("");
+
+    showTask();
+
+};
+
+
+
+
+const editHandler = (identifier) => {
+    const inputForEdit = document.getElementById(`ident${identifier}`);
+    const editBtn = document.getElementById(`edit${identifier}`);
+    const initialValue = document.getElementById(`changedValue${identifier}`);
+    const removeButton = document.getElementById(`remove${identifier}`);
+
+    if (editBtn.innerHTML === 'Edit') {
+        editBtn.innerHTML = "Save";
+        removeButton.innerHTML = "Cancel";
+        initialValue.classList.add("hideItem");
+        inputForEdit.classList.remove("hideItem");
+    } else if (editBtn.innerHTML === "Save" && inputForEdit.value.length > 0) {
+        editBtn.innerHTML = "Edit";
+        removeButton.innerHTML = "Remove";
+        inputForEdit.classList.add("hideItem");
+        initialValue.classList.remove("hideItem");
+        initialValue.innerHTML = inputForEdit.value;
+        initialTaskData.find((data) => {
+            if (data.id === identifier) {
+                data.task = inputForEdit.value;
+            }
+        });
+    }
+
+
+};
+// Item Removing
+const removeBtn = (identifier) => {
+    const removeButton = document.getElementById(`remove${identifier}`);
+
+    initialTaskData = initialTaskData.filter((items) => {
+
+        if (removeButton.innerHTML === "Remove") {
+            return items.id !== identifier;
+        } else {
+            return items;
+        }
+
+    });
+    renderPagesData(initialTaskData);
+    changePage(onPageControler - 1);
+};
+
+const onCheckBox = (identifier) => {
+    const checkBox = document.getElementById(`checkBox${identifier}`);
+    const initialValue = document.getElementById(`changedValue${identifier}`);
+    if (checkBox.checked === true) {
+        initialValue.classList.add('done');
+    } else {
+        initialValue.classList.remove('done');
+    }
+};
